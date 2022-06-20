5a097f705142fdbbeb5699c8ad04193ff4e04d23
linknaka <linknakaema@gmail.com>
2022-06-20 17:50:08 -0300

    Initial commit

---
+++ src/arm/usingRMI.java
@@ -0,0 +1,8 @@
+package arm;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface usingRMI extends Remote {
+   //classe de conexão
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module Survivor {
+	requires java.desktop;
+	requires java.rmi;
+	
+}
